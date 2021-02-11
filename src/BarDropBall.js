e3338fe90b018238d84ea760d9aa77994b208629
Anwesh43 <anweshthecool0@gmail.com>
2021-02-11 22:53:40 +0530

    adding BarDropBall component

---
+++ src/BarDropBall.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useStyle} from './hooks'
+
+const BarDropBall = ({w, h, scale, onClick}) => {
+    const {circleStyle, lineStyle} = useStyle(w, h, scale)
+    return (
+        <div>
+            <div onClick={onClick} style = {circleStyle()}></div>
+            <div style = {lineStyle()}></div>
+        </div>
+    )
+}
+
+export default BarDropBall 
